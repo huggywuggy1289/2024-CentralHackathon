95e28a0768325b2a9d9f1c10efd622c056695a14
Jiye-Park01 <jiye0710@naver.com>
2024-07-23 17:07:24 +0900

    마이페이지 일부 완성

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('welcomepage/', welcomepage.as_view(), name='welcomepage'),  # 새로운 URL 패턴 추가
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name ='logout'),
+    path('mypage/', mypage_view, name="mypage"),
+    path('profile/', profile_view, name="profile"),
 ]
